16f906510d5c30dfc95480a013da55355ee9c3e2
Putr7 <149395927+Putr7@users.noreply.github.com>
2025-11-11 16:02:41 +0700

    Update sll.h

--- sll.h
+++ sll.h
@@ -44,7 +44,7 @@ void printList(List a);
 // Deklarasi subprogram yang diminta
 void insertSorted(List &a, address p);
 void printByUsername(List a, string username);
-address findMostPopular(List a);
-void updateLikes(List &a, int postID, bool isLike);
+address Popular(List a);
+void likes(List &a, int postID, bool isLike);
 
 
